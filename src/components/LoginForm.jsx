00d479d166c8dbf95c19bed1c936d1dcd0b56925
Atefeh Etemadipour <atefeh.etemadipour@gmail.com>
2025-06-12 23:09:43 +0200

    Created Login page

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+
+const LoginForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLogin, setIsLogin] = useState(true);
+  const [error, setError] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!email || !password) {
+      setError("Please fill in all fields");
+      return;
+    }
+    if (!isLogin && password !== confirmPassword) {
+      alert("Passwords do not match");
+      return;
+    }
+    setError("");
+  };
+
+  return (
+    <div className="login-container">
+      <form className="login-form" onSubmit={handleSubmit}>
+        <h2>{isLogin ? "Login" : "Sign Up"}</h2>
+        {error && <div style={{ color: "red" }}>{error}</div>}
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+
+        {!isLogin && (
+          <div>
+            <label htmlFor="confirm-password">Confirm Password:</label>
+            <input
+              type="password"
+              id="confirm-password"
+              name="confirm-password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              required
+            />
+          </div>
+        )}
+        <button type="submit">{isLogin ? "Login" : "Sign Up"}</button>
+
+        <div>
+          <p className="toggle-text">
+            {isLogin ? "Don't have an account?" : "Already have an account?"}{" "}
+            <a className="toggle-link" href="#" onClick={() => setIsLogin(!isLogin)}>
+              {isLogin ? "Sign Up" : "Login"}
+            </a>
+          </p>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
